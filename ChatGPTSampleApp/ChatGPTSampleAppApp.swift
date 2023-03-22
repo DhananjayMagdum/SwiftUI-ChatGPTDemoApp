8f131028f52f0ae87c03b17479596866d0c6f3f3
Dhananjay Magdum <dhananjay.magdum@atlascopco.com>
2023-03-22 17:38:40 +0530

    Initial Commit

---
+++ ChatGPTSampleApp/ChatGPTSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTSampleAppApp.swift
+//  ChatGPTSampleApp
+//
+//  Created by Dhananjay Magdum on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTSampleAppUITests/ChatGPTSampleAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ChatGPTSampleAppUITestsLaunchTests.swift
+//  ChatGPTSampleAppUITests
+//
+//  Created by Dhananjay Magdum on 22/03/23.
+//
+
+import XCTest
+
+final class ChatGPTSampleAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
