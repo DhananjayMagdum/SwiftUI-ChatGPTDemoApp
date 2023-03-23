4a6fbaec59a50344c286330aa5142b53c72c5fa2
Dhananjay Magdum <dhananjay.magdum@atlascopco.com>
2023-03-23 09:19:36 +0530

    add all files

--- ChatGPTSampleApp/ContentView.swift
+++ ChatGPTSampleApp/ContentView.swift
@@ -6,16 +6,69 @@
 //
 
 import SwiftUI
+import OpenAISwift
 
+struct QuestionAnswers: Identifiable {
+    let id = UUID()
+    
+    let question: String
+    let answer: String
+}
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    let openAI =  OpenAISwift(authToken: "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")
+    @State private var search = ""
+    @State private var questionAnswers: [QuestionAnswers] = []
+    @State private var searching = false
+    
+    private func performOpenAISearch() {
+        openAI.sendCompletion(with: search) { result in
+            switch result {
+            case .success(let success):
+                let questionAnswer = QuestionAnswers(question: search, answer: success.choices.first?.text.trimmingCharacters(in: .whitespacesAndNewlines) ?? "")
+                questionAnswers.append(questionAnswer)
+                search = ""
+                searching = false
+            case .failure(let error):
+                print(error.localizedDescription)
+                searching = false
+            }
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView(showsIndicators: false) {
+                                    ForEach(questionAnswers) { qa in
+                                        VStack(spacing: 10) {
+                                            Text(qa.question)
+                                                .bold()
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                            
+                                            Text(qa.answer)
+                                                .padding([.bottom], 10)
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                        }
+                                    }
+                                }.padding()
+                
+                HStack {
+                    TextField("Type here...", text: $search)
+                    .onSubmit {
+                        if !search.isEmpty {
+                            searching = true
+                            performOpenAISearch()
+                        }
+                    }
+                    .padding()
+                    if(searching) {
+                        ProgressView()
+                            .padding()
+                    }
+                }
+            }
+            .navigationTitle("ChatGPT")
+        }
     }
 }
 
